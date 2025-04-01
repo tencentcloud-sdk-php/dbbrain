8d87da24f5a4620dc0e854874ee9fcc1dd0f1123
tencentcloudapi <tencentcloudapi@tencent.com>
2025-04-02 04:41:06 +0800

    release 3.0.1352

--- src/TencentCloud/Dbbrain/V20210527/Models/DescribeSlowLogQueryTimeStatsRequest.php
+++ src/TencentCloud/Dbbrain/V20210527/Models/DescribeSlowLogQueryTimeStatsRequest.php
@@ -18,24 +18,24 @@ namespace TencentCloud\Dbbrain\V20210527\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * DescribeSlowLogTimeSeriesStats请求参数结构体
+ * DescribeSlowLogQueryTimeStats请求参数结构体
  *
  * @method string getInstanceId() 获取实例 ID 。
  * @method void setInstanceId(string $InstanceId) 设置实例 ID 。
  * @method string getStartTime() 获取开始时间，如“2019-09-10 12:13:14”。
  * @method void setStartTime(string $StartTime) 设置开始时间，如“2019-09-10 12:13:14”。
- * @method string getEndTime() 获取结束时间，如“2019-09-10 12:13:14”，结束时间与开始时间的间隔最大可为7天。
- * @method void setEndTime(string $EndTime) 设置结束时间，如“2019-09-10 12:13:14”，结束时间与开始时间的间隔最大可为7天。
- * @method string getProduct() 获取服务产品类型，支持值包括： "mysql" - 云数据库 MySQL， "cynosdb" - 云数据库 CynosDB  for MySQL，"redis" - 云数据库 Redis，"mongodb" - 云数据库 MongoDB，默认为"mysql"。
- * @method void setProduct(string $Product) 设置服务产品类型，支持值包括： "mysql" - 云数据库 MySQL， "cynosdb" - 云数据库 CynosDB  for MySQL，"redis" - 云数据库 Redis，"mongodb" - 云数据库 MongoDB，默认为"mysql"。
- * @method string getInstanceProxyId() 获取Proxy节点ID。	
- * @method void setInstanceProxyId(string $InstanceProxyId) 设置Proxy节点ID。	
- * @method string getInstanceNodeId() 获取实列节点ID。	
- * @method void setInstanceNodeId(string $InstanceNodeId) 设置实列节点ID。	
+ * @method string getEndTime() 获取截止时间，如“2019-09-11 10:13:14”，截止时间与开始时间的间隔小于7天。
+ * @method void setEndTime(string $EndTime) 设置截止时间，如“2019-09-11 10:13:14”，截止时间与开始时间的间隔小于7天。
+ * @method string getProduct() 获取"mysql" - 云数据库 MySQL， "cynosdb" - 云数据库 TDSQL-C for MySQL，"redis" - 云数据库 Redis，"mongodb" - 云数据库 MongoDB，默认为"mysql"。
+ * @method void setProduct(string $Product) 设置"mysql" - 云数据库 MySQL， "cynosdb" - 云数据库 TDSQL-C for MySQL，"redis" - 云数据库 Redis，"mongodb" - 云数据库 MongoDB，默认为"mysql"。
+ * @method string getInstanceProxyId() 获取Proxy节点ID。
+ * @method void setInstanceProxyId(string $InstanceProxyId) 设置Proxy节点ID。
+ * @method string getInstanceNodeId() 获取实列节点ID。
+ * @method void setInstanceNodeId(string $InstanceNodeId) 设置实列节点ID。
  * @method string getType() 获取查询类型，目前支持值：mongod，mongos。
  * @method void setType(string $Type) 设置查询类型，目前支持值：mongod，mongos。
  */
-class DescribeSlowLogTimeSeriesStatsRequest extends AbstractModel
+class DescribeSlowLogQueryTimeStatsRequest extends AbstractModel
 {
     /**
      * @var string 实例 ID 。
@@ -48,22 +48,22 @@ class DescribeSlowLogTimeSeriesStatsRequest extends AbstractModel
     public $StartTime;
 
     /**
-     * @var string 结束时间，如“2019-09-10 12:13:14”，结束时间与开始时间的间隔最大可为7天。
+     * @var string 截止时间，如“2019-09-11 10:13:14”，截止时间与开始时间的间隔小于7天。
      */
     public $EndTime;
 
     /**
-     * @var string 服务产品类型，支持值包括： "mysql" - 云数据库 MySQL， "cynosdb" - 云数据库 CynosDB  for MySQL，"redis" - 云数据库 Redis，"mongodb" - 云数据库 MongoDB，默认为"mysql"。
+     * @var string "mysql" - 云数据库 MySQL， "cynosdb" - 云数据库 TDSQL-C for MySQL，"redis" - 云数据库 Redis，"mongodb" - 云数据库 MongoDB，默认为"mysql"。
      */
     public $Product;
 
     /**
-     * @var string Proxy节点ID。	
+     * @var string Proxy节点ID。
      */
     public $InstanceProxyId;
 
     /**
-     * @var string 实列节点ID。	
+     * @var string 实列节点ID。
      */
     public $InstanceNodeId;
 
@@ -75,10 +75,10 @@ class DescribeSlowLogTimeSeriesStatsRequest extends AbstractModel
     /**
      * @param string $InstanceId 实例 ID 。
      * @param string $StartTime 开始时间，如“2019-09-10 12:13:14”。
-     * @param string $EndTime 结束时间，如“2019-09-10 12:13:14”，结束时间与开始时间的间隔最大可为7天。
-     * @param string $Product 服务产品类型，支持值包括： "mysql" - 云数据库 MySQL， "cynosdb" - 云数据库 CynosDB  for MySQL，"redis" - 云数据库 Redis，"mongodb" - 云数据库 MongoDB，默认为"mysql"。
-     * @param string $InstanceProxyId Proxy节点ID。	
-     * @param string $InstanceNodeId 实列节点ID。	
+     * @param string $EndTime 截止时间，如“2019-09-11 10:13:14”，截止时间与开始时间的间隔小于7天。
+     * @param string $Product "mysql" - 云数据库 MySQL， "cynosdb" - 云数据库 TDSQL-C for MySQL，"redis" - 云数据库 Redis，"mongodb" - 云数据库 MongoDB，默认为"mysql"。
+     * @param string $InstanceProxyId Proxy节点ID。
+     * @param string $InstanceNodeId 实列节点ID。
      * @param string $Type 查询类型，目前支持值：mongod，mongos。
      */
     function __construct()
